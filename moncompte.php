4a676de24a6325cee0329041f76079fa7325ba2b
Sosomanass <solene.43colomb@gmail.com>
2023-12-19 20:56:09 +0100

    creation d'onglet sur la page compte ( interactive )

--- moncompte.php
+++ moncompte.php
@@ -21,14 +21,70 @@ include('presset/header.php');
 <body>
 
     <div class="grayPart"></div>
-    <div class="formInscription">
 
+    <div class="titre">
         <h1 class="h1">Mon compte</h1>
-
-        <p>Changer de mot de passe</p>
     </div>
 
+    <div class="tab">
+        <button class="tablinks" onclick="openTab(event, 'Personnel')">Paramètres du profil</button>
+        <button class="tablinks" onclick="openTab(event, 'Securite')">Mot de passe & sécurité</button>
+    </div>
+    <div id="Personnel" class="tabcontent">
+        <h3 class="titreTabcontent">Modification des informations personnelles</h3>
+        <form>
+            <div class="form-group">
+                <label for="nom">Nom</label>
+                <input type="text" id="nom">
+            </div>
+            <div class="form-group">
+                <label for="prenom">Prénom</label>
+                <input type="text" id="prenom">
+            </div>
+            <div class="form-group">
+                <label for="prenom">Email</label>
+                <input type="text" id="prenom">
+            </div>
+            <div class="form-group">
+                <label for="prenom">Numéro de téléphone</label>
+                <input type="text" id="prenom">
+            </div>
+            <div class="form-group">
+                <label for="prenom">Date de naissance</label>
+                <input type="text" id="prenom">
+            </div>
+            <!-- Répétez pour les autres champs -->
+        </form>
+
+    </div>
+    <div id="Securite" class="tabcontent">
+        <h3 class="titreTabcontent">Changer de mot de passe</h3>
+    </div>
+
+    <?PHP include('presset/footer.php') ?>
+
 </body>
+<script>
+    function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+
+    document.getElementsByClassName('tablinks')[0].click();
+
+</script>
 
 </html>
 <?PHP
@@ -38,5 +94,24 @@ if (isset($_SESSION['prenom'])) {
 } else {
     header("Location: connexion.php");
     exit;
+}
+?>
+
+
+<?PHP
+include('presset/option.php');
+
+$stmt = $conn->prepare("SELECT * FROM utilisateurs WHERE id_utilisateur = :id");
+$stmt->bindParam(':id', $_SESSION['id_utilisateur']);
+$stmt->execute();
+$resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($stmt->rowCount() > 0) {
+
+    echo $resultat['nom'];
+    echo $resultat['prenom'];
+    echo $resultat['mail'];
+    echo $resultat['numero'];
+    echo $resultat['date_de_naissance'];
+
 }
 ?>
